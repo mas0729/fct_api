3761d387e53ce5045744b0e7a3976daa9ed3b687
SasakiMasaru <designer.sasaki.masaru@gmail.com>
2021-12-11 20:48:35 +0900

    初回コミット

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+
+app = FastAPI()
+
+
+class User(BaseModel):
+    name: str
+    age: int
+    mail: Optional[str] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.post("/user/")
+def create_user(user: User):
+    print(user)
+    return {"status": "OK", "name": user.name, "age": user.age}
